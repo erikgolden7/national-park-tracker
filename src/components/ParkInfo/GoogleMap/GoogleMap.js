506ca8e2a9b09a76a827796ef28c42c570b9af46
Erik Golden <erikgolden7@gmail.com>
2018-04-25 19:30:22 -0500

    organized park information and finished google map basics

--- src/components/ParkInfo/GoogleMap/GoogleMap.js
+++ src/components/ParkInfo/GoogleMap/GoogleMap.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import {Map, Marker, GoogleApiWrapper, InfoWindow} from 'google-maps-react';
 import axios from "axios"
 import { toLong } from 'ip';
+import {Link} from "react-router-dom"
 require('dotenv').config()
+
+const LoadingContainer = (props) => (
+  <div>Fancy loading container!</div>
+)
 
 class GoogleMap extends Component {
 constructor(props) {
@@ -12,7 +17,10 @@ constructor(props) {
     map: '',
     lat: '',
     lng: '',
-    zoom: 10
+    zoom: 10,
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
   }
 }
 
@@ -27,39 +35,56 @@ componentDidMount() {
 findLatLong(){
   let lat = '';
   let lng = '';
-  const str = this.props.latLong.split('')
+  const str = this.props.state.latLong.split('')
   lat = str.splice(4, 8);
   lng = str.splice(14, 8);
   this.setState({lat: Number(lat.join('')), lng: Number(lng.join(''))})
 }
 
-onMarkerClick(props, marker, e){
-  console.log(e);
-  console.log(props);
-  console.log(marker);
-  
-  
-  
-}
+// onMarkerClick(props, marker, e){
+//   window.open(props.url), '_blank';
+// }
+
+onMarkerClick = (props, marker, e) =>{
+console.log(props);
+
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
 
   render() {
     const style = {
-      width: '500px',
-      height: '400px'
+      position: 'relative',
+      width: '75%',
+      height: '500px'
     }
     console.log(this.state.lat, this.state.lng)
     return (
-      <Map google={this.props.google} zoom={this.state.zoom} style={style} center={{lat: this.state.lat, lng: this.state.lng}} >
+      <Map google={this.props.google} zoom={this.state.zoom} style={style} center={{lat: this.state.lat, lng: this.state.lng}} mapTypeId='terrain' onClick={this.onMapClicked} >      
         <Marker
+          url={this.props.state.url}
           title={'Select to view NPS website data.'}
-          name={this.props.name}
+          name={this.props.state.name}
           position={{lat: this.state.lat, lng: this.state.lng}} 
           onClick={this.onMarkerClick}
         />  
-
-        <InfoWindow onClose={this.onInfoWindowClose}>
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
             <div>
-              <h1>place</h1>
+              <h1>{this.state.selectedPlace.name}</h1>
             </div>
         </InfoWindow>
       </Map>
@@ -68,5 +93,6 @@ onMarkerClick(props, marker, e){
 }
 
 export default GoogleApiWrapper({
-  apiKey: process.env.REACT_APP_MAP_KEY
+  apiKey: process.env.REACT_APP_MAP_KEY,
+  LoadingContainer: LoadingContainer
 })(GoogleMap)
--- src/components/ParkInfo/ParkInfo.js
+++ src/components/ParkInfo/ParkInfo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import GoogleMap from "./GoogleMap/GoogleMap"
+import {Link} from "react-router-dom"
 import "./parkInfo.css"
 
 export default class ParkInfo extends Component {
@@ -23,7 +24,7 @@ export default class ParkInfo extends Component {
 
   render() {
 
-    const {description, designation, id, latLong, name, parkCode, states, url, weatherInfo} = this.props.state;
+    const {description, designation, id, latLong, name, parkCode, states, url, weatherInfo, fullName} = this.props.state;
     
     const alert = this.state.alerts.map((e, i) => {  
       console.log(e, e.title, e.category);
@@ -102,56 +103,19 @@ export default class ParkInfo extends Component {
         {
           //parkCode &&
           // <img style={{textAlign:"left"}} src={require(`../../assets/parks/${parkCode}.jpg`)} alt="Park Image" className="img-responsive"/>
-
         }
         <div className="text">{description}</div>
-
-        <div>{designation}</div>
-        <GoogleMap name={name} latLong={latLong} />
-        
-        <div>{id}</div>
-        <div>{latLong}</div>
-        <div>{name}</div>
-        <div>{parkCode}</div>
-        <div>{states}</div>
-        <div>{url}</div>
-        <div>{weatherInfo}</div>
+        <div className="large-title-text" >Weather Information:</div>
+        <div className="text">{weatherInfo}</div>
+        <div className="large-title-text"> Additional Information: </div>
+        <div className="text">
+        States: <span> {states} </span>
+        </div>
+        <div className="text">
+        {name} Website: <span> <a target="_blank" href={url}>{url}</a> </span>
+        </div>
+        <GoogleMap state={this.props.state} style={{width:"75%", height: "auto", marginTop:30}} /> 
       </div>
     );
   }
 }
-
-// description
-// :
-// "Within the dry and windswept high plains of the Texas Panhandle lies a hidden oasis, a welcoming haven where wildlife and humans find respite from the dry grasslands above. Through this plain, the Canadian River has cut dramatic 200-foot canyons, or breaks, where humans have eked out a living for over 13,000 years. Lake Meredith now occupies these hidden coves where early humans once roamed."
-// designation
-// :
-// "National Recreation Area"
-// directionsInfo
-// :
-// "There are several different ways to get to Lake Meredith. If you are coming from Amarillo, exit Loop 335 North, then exit Hwy 136 North to Fritch. Headquarters is located at 419 E. Broadway in Fritch. If coming from the North you can take 1913 from the Dumas Highway or 1913 from US 287.↵↵The airport in Amarillo is approximately 35 miles south of Lake Meredith if traveling by air. Rental cars are available."
-// directionsUrl
-// :
-// "http://www.nps.gov/lamr/planyourvisit/directions.htm"
-// fullName
-// :
-// "Lake Meredith National Recreation Area"
-// id
-// :
-// "AC3B46AA-B4C2-4555-A82A-D4CF790DF81D"
-// latLong
-// :
-// "lat:35.61926203, long:-101.6820066"
-// name
-// :
-// "Lake Meredith"
-// parkCode
-// :
-// "lamr"
-// states
-// :
-// "TX"
-// url
-// :
-// "https://www.nps.gov/lamr/index.htm"
-// weatherInfo
